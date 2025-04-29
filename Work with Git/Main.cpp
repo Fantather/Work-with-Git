b7dd8d0f18b77a6a8f283d17c0877b9edd3a237e
Алексей Дириволков <dirivolkova@gmail.com>
2025-04-29 12:18:38 +0300

    привіт з mybranch

--- Work with Git/Main.cpp
+++ Work with Git/Main.cpp
@@ -1,1 +1,12 @@
 #include "Header.h"
+// привіт з mybranch
+
+
+
+
+//1.Створити репо вашого проекту локально, створити вітку mybranch, в якій пишемо код в main() : привіт з mybranch.commit цей стан в mybranch.
+//Переходимо в master, створюємо новій.h файл з будь - яким кодом, робтимо commit.
+//Робимо злиття  mybranch  в master  in VS, вирішуємо конфлікти.
+//Робимо commit в master і push на git.hub.
+//
+//2.Робимо злиття з master в mybranch  in VS, commit, push вітки і приймаєте на git.hub
